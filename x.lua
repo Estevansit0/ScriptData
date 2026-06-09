e101503f7a22f84dc6d4acfcc929acbeb8f8176f
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2026-06-09 10:07:49 -0700

    Create x.lua

---
+++ x.lua
@@ -0,0 +1,297 @@
+local ToolData = {
+    ["BlackLegStyle"] = {
+        Skills = {
+            ["Diable Jambe"] = {
+                Cooldown = 5
+            },
+            ["Spectre"] = {
+                Cooldown = 13
+            },
+            ["Party Table Kick"] = {
+                Cooldown = 10
+            },
+            ["Anti-Manner Kick"] = {
+                Cooldown = 5
+            },
+        }
+    },
+    ["Combat"] = {
+        Skills = {
+            ["Ultimate Upper-Cut"] = {
+                Cooldown = 12
+            },
+            ["Strong-Punch"] = {
+                Cooldown = 7
+            },
+        }
+    },
+    ["FishmanKarate"] = {
+        Skills = {
+            ["Aqua Blast"] = {
+                Cooldown = 7
+            },
+            ["Fishman Awakening"] = {
+                Cooldown = 5
+            },
+            ["Aqua Beam"] = {
+                Cooldown = 15
+            },
+            ["Aqua Bomb"] = {
+                Cooldown = 12
+            },
+        }
+    },
+    ["NightGuyStyle"] = {
+        Skills = {
+            ["Youth Kick"] = {
+                Cooldown = 8
+            },
+            ["The Eighth Gate"] = {
+                Cooldown = 10
+            },
+            ["Asakujaku"] = {
+                Cooldown = 18
+            },
+            ["Raging Sekizo"] = {
+                Cooldown = 13
+            },
+            ["Front Lotus"] = {
+                Cooldown = 7
+            },
+            ["Night Guy"] = {
+                Cooldown = 25
+            },
+        }
+    },
+    ["Katana"] = {
+        Skills = {
+            ["Rapid Slash"] = {
+                Cooldown = 7
+            },
+            ["Slash Barrage"] = {
+                Cooldown = 12
+            },
+        }
+    },
+    ["DarkBlade"] = {
+        Skills = {
+            ["Thousand Cuts"] = {
+                Cooldown = 7
+            },
+            ["Dark Slash"] = {
+                Cooldown = 13
+            },
+        }
+    },
+    ["2SS"] = {
+        Skills = {
+            ["Rashamon"] = {
+                Cooldown = 7
+            },
+            ["Dragon Twister"] = {
+                Cooldown = 13
+            },
+        }
+    },
+    ["ZankokuBlade"] = {
+        Skills = {
+            ["Zankoku: Oblivion Cut"] = {
+                Cooldown = 7
+            },
+            ["Zankoku: Abyss Flurry"] = {
+                Cooldown = 13
+            },
+        }
+    },
+    ["AxeHand"] = {
+        Skills = {
+            ["Axe-Spin"] = {
+                Cooldown = 13
+            },
+            ["Axe-Smash"] = {
+                Cooldown = 7
+            },
+        }
+    },
+    ["Tojih"] = {
+        Skills = {
+            ["Sever Chain"] = {
+                Cooldown = 22
+            },
+            ["Execution Dash"] = {
+                Cooldown = 12
+            },
+        }
+    },
+    ["KidBuu"] = {
+        Skills = {
+            ["Majin Kamehameha"] = {
+                Cooldown = 7
+            },
+            ["Majin Roar"] = {
+                Cooldown = 5
+            },
+            ["True Majin"] = {
+                Cooldown = 10
+            },
+            ["Super Vanishing Ball"] = {
+                Cooldown = 14
+            },
+            ["Assault Rain"] = {
+                Cooldown = 10
+            },
+        }
+    },
+    ["GokoSSJ4"] = {
+        Skills = {
+            ["Super Saiyan 4"] = {
+                Cooldown = 10
+            },
+            ["Super Combo"] = {
+                Cooldown = 14
+            },
+            ["Bursting Energy"] = {
+                Cooldown = 10
+            },
+            ["10x Kamehameha"] = {
+                Cooldown = 7
+            },
+            ["Dragon Fist"] = {
+                Cooldown = 50
+            },
+            ["Strong Punch"] = {
+                Cooldown = 10
+            },
+        }
+    },
+    ["Itacho"] = {
+        Skills = {
+            ["Tsukayomi"] = {
+                Cooldown = 15
+            },
+            ["Ultimate Black Flames"] = {
+                Cooldown = 20
+            },
+            ["Crow Dash"] = {
+                Cooldown = 10
+            },
+            ["Shuriken Jutsu Rainfall"] = {
+                Cooldown = 7
+            },
+        }
+    },
+    ["Kizaro"] = {
+        Skills = {
+            ["Judgment Day: Rayfall"] = {
+                Cooldown = 20
+            },
+            ["Light Flight"] = {
+                Cooldown = 0.5
+            },
+            ["Light Kick"] = {
+                Cooldown = 10
+            },
+            ["Light Beam"] = {
+                Cooldown = 15
+            },
+        }
+    },
+    ["Gaaro"] = {
+        Skills = {
+            ["Sand Bullets"] = {
+                Cooldown = 8
+            },
+            ["Quick Sand"] = {
+                Cooldown = 15
+            },
+            ["Sand Pyramid"] = {
+                Cooldown = 20
+            },
+        }
+    },
+    ["KidGohon"] = {
+        Skills = {
+            ["Masenko"] = {
+                Cooldown = 15
+            },
+            ["Ki Barrage"] = {
+                Cooldown = 8
+            },
+        }
+    },
+    ["Natso"] = {
+        Skills = {
+            ["Fire Dragons Pillar"] = {
+                Cooldown = 7
+            },
+            ["Fire Dragons Surge"] = {
+                Cooldown = 30
+            },
+        }
+    },
+    ["Gojoh"] = {
+        Skills = {
+            ["Maximum: Blue"] = {
+                Cooldown = 18
+            },
+            ["Reversal Red"] = {
+                Cooldown = 13
+            },
+            ["Limitless Pulse"] = {
+                Cooldown = 8
+            },
+            ["Hollow Purple"] = {
+                Cooldown = 25
+            },
+            ["Awakened Limitless"] = {
+                Cooldown = 10
+            },
+            ["Unstable Red"] = {
+                Cooldown = 10
+            },
+        }
+    },
+    ["TitanEron"] = {
+        Skills = {
+            ["The Rumbling"] = {
+                Cooldown = 5
+            },
+            ["Titan Shift"] = {
+                Cooldown = 5
+            },
+        }
+    },
+    ["Katakuro"] = {
+        Skills = {
+            ["Mochi Barrage"] = {
+                Cooldown = 11
+            },
+            ["Buzzcut Mochi"] = {
+                Cooldown = 30
+            },
+            ["Burning Mochi"] = {
+                Cooldown = 7
+            },
+            ["Mochi Floor"] = {
+                Cooldown = 6
+            },
+            ["Mochi Awakening"] = {
+                Cooldown = 10
+            },
+            ["Chestnut Mochi"] = {
+                Cooldown = 10
+            },
+        }
+    },
+    ["CoyoteSharksPistols"] = {
+        Skills = {
+            ["Bala"] = {
+                Cooldown = 7
+            },
+            ["Cero Metralleta"] = {
+                Cooldown = 13
+            },
+        }
+    },
+}
+return ToolData

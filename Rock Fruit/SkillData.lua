73cacbd4e558dbf9e6ac4e489e352cefc8b39552
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2026-07-14 11:24:48 -0700

    Create SkillData.lua

---
+++ Rock Fruit/SkillData.lua
@@ -0,0 +1,178 @@
+-- Script Path: game:GetService("ReplicatedStorage").Modules.SkillList
+return {
+    Combat = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Sand = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    ["Black Leg"] = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Bomb = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Shirasaya = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Boxing = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Kitetsu = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Lightning = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    ["Karate Fish"] = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Ryusoken = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Shisui = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Quake = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Kiribachi = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Smoke = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Katana = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Enma = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Snow = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Gas = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Yoru = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Rokushiki = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Light = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Wado = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Fire = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Tanto = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    Ice = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    ["Santa Demon"] = {
+        "Z", 
+        "X", 
+        "C", 
+        "V", 
+        "F"
+    }, 
+    ["Super Bacon"] = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    ["Michael Jackson"] = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }, 
+    ["Aura Monster"] = {
+        "Z", 
+        "X", 
+        "C", 
+        "V"
+    }
+};

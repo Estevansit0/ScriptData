921b4915893db9e3e7084a412d249b8d28e284c8
Estevansito <160373638+Estevansit0@users.noreply.github.com>
2026-07-17 11:58:26 -0700

    Create ClassData.lua

---
+++ Rock Fruit/ClassData.lua
@@ -0,0 +1,14 @@
+return {
+    "Experience",
+    "Stronger",
+    "Skypieans",
+    "Celestial Dragons",
+    "Merfolk",
+    "Vampire",
+    "Re:Zero",
+    "I have no enemies",
+    "Viltrumite",
+    "Adaptation",
+    "Edgerunner",
+    "The Eminence in Shadow"
+}
